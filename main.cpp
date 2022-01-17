7bf1b5f155f1181bacc8e1f14068bd010fd8ea03
Trinity <shaky_dlo_680@hotmail.com>
2022-01-16 22:32:18 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Game Over 3.0
+// Demonstrates using declarations
+
+// Includes the input/output stream
+#include <iostream>
+// Declarations that allow programmer to use cout without typing std::
+using std::cout;
+using std::endl;
+
+// Main function
+int main()
+{
+	// Outputs 'Game On!' to the console
+	cout << "Game On!" << endl;
+
+	// Returns 0 (Main function needs to return an integer)
+	return 0;
+}
